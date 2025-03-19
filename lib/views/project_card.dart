53b138c2d3b8effaa1f3607b4efa534eccdb2b5a
Ravad Nadam <0rwad.nadam0@gmail.com>
2025-03-19 22:05:26 +0300

    Did minor adjecments to task card

--- lib/views/project_card.dart
+++ lib/views/project_card.dart
@@ -3,21 +3,54 @@ import 'package:salam_hackathon_front/models/models.dart';
 import 'package:salam_hackathon_front/views/project_page.dart';
 
 class ProjectCard extends StatelessWidget {
-  const ProjectCard({super.key, required this.p, required this.language, required this.framework});
+  const ProjectCard(
+      {super.key,
+      required this.p,
+      required this.language,
+      required this.framework});
 
-  final Project p ;
+  final Project p;
   final String language, framework;
+
+  Widget buildProgressBar(int progress, int totalSteps) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      textDirection: TextDirection.rtl,
+      children: List.generate(totalSteps * 2 - 1, (index) {
+        if (index % 2 == 0) {
+          bool isCompleted = index ~/ 2 < progress;
+          return Icon(
+            Icons.diamond,
+            color: isCompleted ? Colors.yellow : Colors.grey,
+            size: 16,
+          );
+        } else {
+          bool isPreviousCompleted = (index ~/ 2) < progress;
+          return Expanded(
+            child: Container(
+              height: 4,
+              color: isPreviousCompleted ? Colors.blue : Colors.grey[300],
+            ),
+          );
+        }
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: p.isLocked ? (){} : () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ProjectPage(p: p, language: language, framework: framework),
-          ),
-        );
-      },
+      onTap: p.isLocked
+          ? () {}
+          : () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProjectPage(
+                      p: p, language: language, framework: framework),
+                ),
+              );
+            },
       child: Stack(
         children: [
           Container(
@@ -39,8 +72,18 @@ class ProjectCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(p.title, style: TextStyle(fontSize: 20),),
-                Text(p.description, style: TextStyle(fontSize: 16,),textAlign: TextAlign.end,maxLines: 3),
+                Text(
+                  p.title,
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(p.description,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.end,
+                    maxLines: 3),
+                const SizedBox(height: 8),
+                buildProgressBar(p.progress, p.tasknum),
               ],
             ),
           ),
@@ -52,7 +95,10 @@ class ProjectCard extends StatelessWidget {
                 color: Colors.grey,
                 borderRadius: BorderRadius.circular(8),
               ),
-              child: Icon(Icons.lock, color: Colors.white,),
+              child: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
             ),
           ),
         ],
